a53ada2531aa27f2e74aad9810802aefc9bb2226
samik-ko <worksojobs@gmail.com>
2025-05-15 16:31:34 +0700

    test commit

--- Git_1.py
+++ Git_1.py
@@ -3,3 +3,15 @@
 print('Hello World!')
 print('Hello World!')
 print('Hello World!')
+
+# init | --global name and mail
+# git branch -M main  # change branch 
+# git remote add origin https://github.com/samik-ko/test_01.git
+# push -u origin main
+# ---
+# Git clone name_reepository <= копирует с GitHub репозиторий на ПК 
+
+
+
+
+stop = '35min'
